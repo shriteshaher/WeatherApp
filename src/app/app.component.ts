7d428c0f1eb24447ab19221c5e5501eb0e87324f
Shritesh Aher <shriteshaher@gmail.com>
2022-04-11 10:02:16 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CallApiService } from './call-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Weather';
+  weather:any
+  t1:boolean=true
+  t2:boolean=false
+  constructor(private callApi:CallApiService){
+     this.callApi.getApi("Nashik").subscribe(res=>{
+      this.weather=res
+      console.log(res)
+    })
+  }
+  open(t1:boolean,t2:boolean){
+      this.t1=t1
+      this.t2=t2
+  }
+
+  close(t1:boolean,t2:boolean){
+    this.t1=false
+    this.t2=true
+  }
+
+  search1(search:string){
+    console.log(search)
+    if(search!="")
+    this.callApi.getApi(search).subscribe(res=>{
+      this.weather=res
+    })
+    else
+     this.callApi.getApi("Nashik").subscribe(res=>{
+      this.weather=res
+
+    })
+  }
+
 }
---
+++ src/app/call-api.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CallApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getApi(city:string){
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=490a4797cec2fac54e420d085c76df02&&units=metric`)
+  }
+}
